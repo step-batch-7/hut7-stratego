e6a262b3238d5531b027a4c2c7de1143c1c27da5
sukhiboi <41sukhdevsingh@gmail.com>
2020-03-06 20:53:24 +0530

    |#23|sukhiboi/ragini|Added move method in Piece class and updated eslintrc
    What changes you have made?
      - Added move method in Piece class.
      - Updated eslintrc magic number rule
    
    Why this change is necessary?
      - Added move method so that a piece can move to a specific position.
      - Updated no-magic-number rule to warn when there is a magic number.

--- test/testPiece.js
+++ test/testPiece.js
@@ -9,4 +9,10 @@ describe('Piece', () => {
       position: [0, 0]
     });
   });
+
+  it('should update the position of given piece', () => {
+    const piece = new Piece('marshal', [0, 0]);
+    const currentPosition = piece.move([9, 0]);
+    assert.deepStrictEqual(currentPosition, [9, 0]);
+  });
 });
