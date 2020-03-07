70f78fdae8570d2be166e89a338665c7b5747548
sukhiboi <41sukhdevsingh@gmail.com>
2020-03-07 14:59:56 +0530

    |#23|ragini/sukhiboi|Added addPlayer method in Game Class

--- test/testGame.js
+++ test/testGame.js
@@ -1,8 +1,9 @@
 const assert = require('chai').assert;
 const { Game } = require('./../lib/game');
+const { Player } = require('./../lib/player');
 
 describe('Game', () => {
-  context('.getStatus', () => {
+  context('.getStatus()', () => {
     it('should give the status of the game', () => {
       const gameId = 123;
       const game = new Game(gameId);
@@ -20,6 +21,26 @@ describe('Game', () => {
           [6, 5],
           [7, 5]
         ]
+      });
+    });
+
+    context('.addPlayer()', () => {
+      it('should add the given player in Game', () => {
+        const gameId = 123;
+        const game = new Game(gameId);
+        const player = new Player('venky', 'red');
+        const numberOfPlayers = game.addPlayer(player);
+        assert.strictEqual(numberOfPlayers, 1);
+      });
+      it('should not add the given player in Game if player is not an instance of Player class ', () => {
+        const gameId = 123;
+        const game = new Game(gameId);
+        const player = {
+          name: 'venky',
+          unit: 'red'
+        };
+        const numberOfPlayers = game.addPlayer(player);
+        assert.strictEqual(numberOfPlayers, 0);
       });
     });
   });
