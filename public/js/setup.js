741611b1e109aed23a91f48b9a0153fe5540b37d
venkatesh <58026249+venkybavisetti@users.noreply.github.com>
2020-03-07 13:03:58 +0530

    |#17|rajat/venky|added feature to place piece from palette to board
      added handlers for pieceContainer and tiles
      to move a piece from pieceContainer to tile

--- public/js/setup.js
+++ public/js/setup.js
@@ -1,16 +1,121 @@
 const createTerritory = function() {
   const territory = document.querySelector('.territory');
-  for (let row = 3; row >= 0; row--) {
+  for (let row = 4; row > 0; row--) {
     for (let column = 0; column < 10; column++) {
       const tile = document.createElement('div');
       tile.id = `${row}_${column}`;
+      this[tile.id];
       tile.classList.add('tile');
       territory.appendChild(tile);
     }
   }
 };
 
+const createElement = (element, className) => {
+  const htmlElement = document.createElement(element);
+  htmlElement.classList.add(className);
+  return htmlElement;
+};
+
+const createElementWithData = (element, data) => {
+  const htmlElement = document.createElement(element);
+  htmlElement.innerText = data;
+  return htmlElement;
+};
+
+const createImage = (name, className) => {
+  const imagesUrl = {
+    scout: './images/scout.png',
+    bomb: './images/bomb.png',
+    captain: './images/captain.png',
+    colonel: './images/colonel.png',
+    flag: './images/flag.png',
+    general: './images/general.png',
+    lieutenant: './images/lieutenant.png',
+    major: './images/major.png',
+    marshal: './images/marshal.png',
+    miner: './images/miner.png',
+    sergeant: './images/sergeant.png',
+    spy: './images/spy.png'
+  };
+  if (!imagesUrl[name]) {
+    return;
+  }
+  const htmlImgElement = document.createElement('img');
+  htmlImgElement.src = imagesUrl[name];
+  htmlImgElement.classList.add(className);
+  return htmlImgElement;
+};
+
+const appendChildren = function(parent, ...children) {
+  children.forEach(child => parent.appendChild(child));
+};
+
+const createPalette = function(pieces) {
+  const palette = document.querySelector('.palette');
+  pieces.forEach(piece => {
+    const pieceContainer = createElement('div', 'pieceContainer');
+    const pieceImageContainer = createElement('div', 'pieceImageContainer');
+    const pieceImage = createImage(piece.name, 'pieceImage');
+    pieceImageContainer.appendChild(pieceImage);
+    const pieceData = createElement('div', 'pieceData');
+    const pieceName = createElementWithData('span', piece.name);
+    const pieceCount = createElementWithData('span', 'x' + piece.count);
+    appendChildren(pieceData, pieceName, pieceCount);
+    appendChildren(pieceContainer, pieceImageContainer, pieceData);
+    palette.appendChild(pieceContainer);
+  });
+};
+
+const selectPiece = function(setUpInfo, pieceContainer) {
+  setUpInfo.selectedPiece =
+    pieceContainer.lastElementChild.firstElementChild.innerText;
+};
+
+const placePiece = function(setUpInfo, tile) {
+  const position = tile.id;
+  const name = setUpInfo.selectedPiece;
+  setUpInfo.piecesInfo.push({ position, name });
+  const image = createImage(name, 'boardPieceImage');
+  if (!image) {
+    return;
+  }
+  tile.firstElementChild || tile.appendChild(image);
+  setUpInfo.selectedPiece = undefined;
+};
+
+const attachListeners = function() {
+  const pieceContainers = Array.from(
+    document.querySelectorAll('.pieceContainer')
+  );
+  pieceContainers.forEach(pieceContainer => {
+    pieceContainer.onclick = selectPiece.bind(null, this, pieceContainer);
+  });
+
+  const tiles = Array.from(document.querySelectorAll('.tile'));
+  tiles.forEach(tile => {
+    tile.onclick = placePiece.bind(null, this, tile);
+  });
+};
+
 const main = function() {
+  const setUpInfo = { selectedPiece: '', piecesInfo: [] };
   createTerritory();
+  const pieces = [
+    { name: 'marshal', count: 1 },
+    { name: 'scout', count: 2 },
+    { name: 'bomb', count: 2 },
+    { name: 'miner', count: 2 },
+    { name: 'flag', count: 1 },
+    { name: 'colonel', count: 1 },
+    { name: 'general', count: 1 },
+    { name: 'lieutenant', count: 1 },
+    { name: 'sergeant', count: 1 },
+    { name: 'captain', count: 1 },
+    { name: 'major', count: 1 },
+    { name: 'spy', count: 1 }
+  ];
+  createPalette(pieces);
+  attachListeners.bind(setUpInfo)();
 };
 window.onload = main;
