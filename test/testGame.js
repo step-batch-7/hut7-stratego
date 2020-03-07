411430b417b9d8f87eafcf91c3fea8bbd455870d
sukhiboi <41sukhdevsingh@gmail.com>
2020-03-07 21:45:30 +0530

    |#23|ragini/sukhiboi|Updated Game class
    Added
    - arrangeBattleField
    - isTargetOnLake
    - isOccupied
    - updateBattleField
    - movePiece
    
    added utils.js

--- test/testGame.js
+++ test/testGame.js
@@ -6,10 +6,11 @@ describe('Game', () => {
   context('.getStatus()', () => {
     it('should give the status of the game', () => {
       const gameId = 123;
-      const game = new Game(gameId, createBattleFieldTemplate(1, 1));
+      const player = new Player('venky', 'red');
+      const game = new Game(gameId, player, createBattleFieldTemplate(1, 1));
       assert.deepStrictEqual(game.getStatus(), {
         id: 123,
-        players: [],
+        players: [player],
         battleField: { '0_0': undefined },
         lakePositions: [
           [2, 4],
@@ -24,45 +25,45 @@ describe('Game', () => {
       });
     });
   });
+
   context('.addPlayer()', () => {
     it('should add the given player in Game', () => {
-      const game = new Game(123, createBattleFieldTemplate(10, 10));
-      const player = new Player('venky', 'red');
-      const numberOfPlayers = game.addPlayer(player);
-      assert.strictEqual(numberOfPlayers, 1);
+      const player1 = new Player('venky', 'red');
+      const game = new Game(123, player1, createBattleFieldTemplate(10, 10));
+      const player2 = new Player('rajat', 'blue');
+      const numberOfPlayers = game.addPlayer(player2);
+      assert.strictEqual(numberOfPlayers, 2);
     });
-    it('should not add the given player in Game if player is not an instance of Player class ', () => {
-      const game = new Game(123, createBattleFieldTemplate(10, 10));
-      const player = {
+    it('should not add player if its not an instance of Player class ', () => {
+      const player1 = new Player('venky', 'red');
+      const game = new Game(123, player1, createBattleFieldTemplate(10, 10));
+      const player2 = {
         name: 'venky',
         unit: 'red'
       };
-      const numberOfPlayers = game.addPlayer(player);
-      assert.strictEqual(numberOfPlayers, 0);
+      const numberOfPlayers = game.addPlayer(player2);
+      assert.strictEqual(numberOfPlayers, 1);
     });
   });
 
   context('.getPlayer()', () => {
     it('should give player of given unit', () => {
-      const game = new Game(123, createBattleFieldTemplate(10, 10));
       const player = new Player('venky', 'red');
-      game.addPlayer(player);
+      const game = new Game(123, player, createBattleFieldTemplate(10, 10));
       assert.strictEqual(game.getPlayer('red'), player);
     });
-      
+
     it('should give undefined when player of the given unit does not exists', () => {
-      const game = new Game(123, createBattleFieldTemplate(10, 10));
       const player = new Player('venky', 'red');
-      game.addPlayer(player);
+      const game = new Game(123, player, createBattleFieldTemplate(10, 10));
       assert.isUndefined(game.getPlayer('blue'));
     });
   });
 
   context('.arrangeBattleField()', () => {
     it('should record the position of all components of the board', () => {
-      const game = new Game(123, createBattleFieldTemplate(10, 10));
       const player = new Player('venky', 'red');
-      game.addPlayer(player);
+      const game = new Game(123, player, createBattleFieldTemplate(10, 10));
       const setUpInfo = {
         unit: 'red',
         piecesInfo: [{ position: '0_0', name: 'marshal' }]
@@ -71,9 +72,8 @@ describe('Game', () => {
     });
 
     it('should not update the battleField if unit is other than red or blue', () => {
-      const game = new Game(123, createBattleFieldTemplate(10, 10));
-      const player = new Player('venky', 'green');
-      game.addPlayer(player);
+      const player = new Player('venky', 'red');
+      const game = new Game(123, player, createBattleFieldTemplate(10, 10));
       const setUpInfo = {
         unit: 'green',
         piecesInfo: [{ position: '0_0', name: 'marshal' }]
@@ -82,14 +82,129 @@ describe('Game', () => {
     });
 
     it('should not update the battleField if piece is not a game component', () => {
-      const game = new Game(123, createBattleFieldTemplate(10, 10));
       const player = new Player('venky', 'red');
-      game.addPlayer(player);
+      const game = new Game(123, player, createBattleFieldTemplate(10, 10));
       const setUpInfo = {
         unit: 'red',
         piecesInfo: [{ position: '0_0', name: 'cannon' }]
       };
       assert.isFalse(game.arrangeBattleField(setUpInfo));
+    });
+  });
+
+  context('.isTargetOnLake()', () => {
+    it('should return true if target is on Lake', () => {
+      const player = new Player('venky', 'red');
+      const game = new Game(123, player, createBattleFieldTemplate(10, 10));
+      const setUpInfo = {
+        unit: 'red',
+        piecesInfo: [{ position: '2_4', name: 'marshal' }]
+      };
+      game.arrangeBattleField(setUpInfo);
+      assert.isTrue(game.isTargetOnLake([2, 4]));
+    });
+
+    it('should return false if target is not on Lake', () => {
+      const player = new Player('venky', 'red');
+      const game = new Game(123, player, createBattleFieldTemplate(10, 10));
+      const setUpInfo = {
+        unit: 'red',
+        piecesInfo: [{ position: '0_0', name: 'marshal' }]
+      };
+      game.arrangeBattleField(setUpInfo);
+      assert.isFalse(game.isTargetOnLake([1, 4]));
+    });
+  });
+
+  context('.isOccupied()', () => {
+    it('should return true if given position is occupied', () => {
+      const player = new Player('venky', 'red');
+      const game = new Game(123, player, createBattleFieldTemplate(10, 10));
+      const setUpInfo = {
+        unit: 'red',
+        piecesInfo: [{ position: '0_0', name: 'marshal' }]
+      };
+      game.arrangeBattleField(setUpInfo);
+      assert.isTrue(game.isOccupied([0, 0]));
+    });
+
+    it('should return false if given position is not occupied', () => {
+      const player = new Player('venky', 'red');
+      const game = new Game(123, player, createBattleFieldTemplate(10, 10));
+      const setUpInfo = {
+        unit: 'red',
+        piecesInfo: [{ position: '0_0', name: 'marshal' }]
+      };
+      game.arrangeBattleField(setUpInfo);
+      assert.isFalse(game.isOccupied([0, 1]));
+    });
+  });
+
+  context('updateBattleField', () => {
+    it('should update piece from current position to target position', () => {
+      const player = new Player('venky', 'red');
+      const game = new Game(123, player, createBattleFieldTemplate(10, 10));
+      const setUpInfo = {
+        unit: 'red',
+        piecesInfo: [{ position: '0_0', name: 'marshal' }]
+      };
+      game.arrangeBattleField(setUpInfo);
+      game.movePiece('red', '0_0', '0_1');
+      const updatedPosition = game.updateBattleField('0_0', '0_1');
+      assert.deepStrictEqual(updatedPosition, '0_1');
+    });
+  });
+
+  context('.movePiece()', () => {
+    it('should update the position of piece of given player', () => {
+      const player = new Player('venky', 'red');
+      const game = new Game(123, player, createBattleFieldTemplate(10, 10));
+      const setUpInfo = {
+        unit: 'red',
+        piecesInfo: [{ position: '0_0', name: 'marshal' }]
+      };
+      game.arrangeBattleField(setUpInfo);
+      const currentPosition = game.movePiece('red', '0_0', '0_1');
+      assert.deepStrictEqual(currentPosition, [0, 1]);
+    });
+
+    it('should not update the position of the piece if the given target position is a lake', () => {
+      const player = new Player('venky', 'red');
+      const game = new Game(123, player, createBattleFieldTemplate(10, 10));
+      const setUpInfo = {
+        unit: 'red',
+        piecesInfo: [{ position: '1_4', name: 'marshal' }]
+      };
+      game.arrangeBattleField(setUpInfo);
+      const currentPosition = game.movePiece('red', '1_4', '2_4');
+      assert.deepStrictEqual(currentPosition, [1, 4]);
+    });
+
+    it('should not update the position of the piece if the given target position is occupied', () => {
+      const player = new Player('venky', 'red');
+      const game = new Game(123, player, createBattleFieldTemplate(10, 10));
+      const setUpInfo = {
+        unit: 'red',
+        piecesInfo: [
+          { position: '1_4', name: 'marshal' },
+          { position: '2_4', name: 'scout' }
+        ]
+      };
+      game.arrangeBattleField(setUpInfo);
+      const currentPosition = game.movePiece('red', '1_4', '2_4');
+      assert.deepStrictEqual(currentPosition, [1, 4]);
+    });
+
+    it('should not move the piece to diagonal of current position', () => {
+      const player = new Player('venky', 'red');
+      const game = new Game(123, player, createBattleFieldTemplate(10, 10));
+      const setUpInfo = {
+        unit: 'red',
+        piecesInfo: [{ position: '1_4', name: 'marshal' }]
+      };
+      game.arrangeBattleField(setUpInfo);
+      const currentPosition = game.movePiece('red', '1_4', '3_4');
+      assert.deepStrictEqual(currentPosition, [1, 4]);
     });
   });
 });
---
+++ test/testRoutes.js
@@ -0,0 +1,49 @@
+const request = require('supertest');
+const { app } = require('../lib/routes');
+
+describe('GET', () => {
+  context('/game.html', function() {
+    it('should response back with game.html', function(done) {
+      request(app)
+        .get('/game.html')
+        .expect(200)
+        .expect(/Stratego/)
+        .end(function(err) {
+          if (err) {
+            return done(err);
+          }
+          done();
+        });
+    });
+  });
+
+  context('/gameId', function() {
+    it('should response back with gameId for /gameId', function(done) {
+      request(app)
+        .get('/gameId')
+        .expect(200)
+        .end(function(err) {
+          if (err) {
+            return done(err);
+          }
+          done();
+        });
+    });
+  });
+});
+
+describe('POST', () => {
+  context('/hostGame', function() {
+    it('should create game with given player name for /hostGame', done => {
+      request(app)
+        .post('/hostGame')
+        .expect(200)
+        .end(function(err) {
+          if (err) {
+            return done(err);
+          }
+          done();
+        });
+    });
+  });
+});
