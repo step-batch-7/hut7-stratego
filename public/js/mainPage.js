75b4944106bedee1c9bc7b257427c25f637b3937
Apurva <apurva.gurme@thoughtworks.com>
2020-03-07 10:57:51 +0530

    |#13|Apurva/Sravani|Separated home page and host page.
    Removed gitkeep from css
    Made two separate css files for home page and host page.
    Added back option in the host page to come back to homepage.

--- public/js/mainPage.js
+++ public/js/mainPage.js
@@ -1,12 +1,8 @@
-const showHost = function() {
-  document.querySelector('#homePage').style.display = 'none';
-  document.querySelector('#hostGame').style.display = 'block';
-};
-
 const showWaiting = function() {
   const input = document.querySelector('.placeholderColor').value;
   if (input.trim()) {
     document.querySelector('#hostGame').style.display = 'none';
+    document.querySelector('#back').style.display = 'none';
     document.querySelector('#waitingBlock').style.display = 'block';
     return;
   }
