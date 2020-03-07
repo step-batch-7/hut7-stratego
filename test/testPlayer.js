d908b44bb15bab7e3be18c9b72196d6ee76c6a89
sukhiboi <41sukhdevsingh@gmail.com>
2020-03-07 11:24:24 +0530

    |#23|ragini/sukhiboi|Added addPiece method in Player class
    addPiece method adds a given piece in the player's army.

--- test/testPlayer.js
+++ test/testPlayer.js
@@ -1,5 +1,6 @@
 const assert = require('chai').assert;
 const { Player } = require('./../lib/player');
+const { Piece } = require('./../lib/piece');
 
 describe('Player', () => {
   context('.getStatus()', function() {
@@ -10,6 +11,22 @@ describe('Player', () => {
         unit: 'red',
         army: []
       });
+    });
+  });
+
+  context('.addPiece()', function() {
+    it('should add given piece in player army', () => {
+      const player = new Player('venky', 'red');
+      const piece = new Piece('marshal', [0, 0]);
+      assert.strictEqual(player.addPiece(piece), 1);
+    });
+    it('should add given piece in player army', () => {
+      const player = new Player('venky', 'red');
+      const piece = {
+        name: 'scout',
+        position: [9, 9]
+      };
+      assert.strictEqual(player.addPiece(piece), 0);
     });
   });
 });
