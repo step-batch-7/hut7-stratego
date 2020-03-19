1f61cf897321266d305cf37bf8cdd38bc8e54f13
Bernard Nehal D'Cunha <58025656+bernie-walker@users.noreply.github.com>
2020-03-19 22:18:58 +0530

    |#32|bernie/venky|modified Game and Player
    * modified arrangeBattleField in Game to not to create piece but send
    piece info to Player for creating and adding the piece to the Players
    army.
    * added a method getPieceStatus in player

--- test/testPlayer.js
+++ test/testPlayer.js
@@ -1,6 +1,5 @@
 const assert = require('chai').assert;
 const { Player } = require('./../lib/player');
-const { Piece, StablePiece } = require('./../lib/piece');
 
 describe('Player', function() {
   context('.getStatus()', function() {
@@ -17,43 +16,34 @@ describe('Player', function() {
   context('.addPiece()', function() {
     it('should add given piece in player army', function() {
       const player = new Player('venky', 'red');
-      const piece = new Piece('marshal', [0, 0], true);
-      assert.strictEqual(player.addPiece(piece), 1);
-    });
-    it('should not add given piece in player army if piece is not an instance of Piece class', function() {
-      const player = new Player('venky', 'red');
-      const piece = {
-        name: 'scout',
-        position: [9, 9]
-      };
-      assert.strictEqual(player.addPiece(piece), 0);
+      assert.strictEqual(player.addPiece('marshal', [0, 0]), 1);
     });
   });
+
   context('.movePiece()', function() {
     it('should update position of piece to given position if piece exists', function() {
       const player = new Player('venky', 'red');
-      const piece = new Piece('marshal', [0, 0], true);
-      player.addPiece(piece);
+      player.addPiece('marshal', [0, 0]);
       assert.isTrue(player.movePiece([0, 0], [0, 1]));
     });
+
     it('should not update position of piece to given position if piece does not exists', function() {
       const player = new Player('venky', 'red');
-      const piece = new Piece('marshal', [9, 9], true);
-      player.addPiece(piece);
+      player.addPiece('marshal', [9, 9]);
       assert.isFalse(player.movePiece([0, 0], [0, 1]));
     });
+
     it('should not update position of piece to given position if piece is immovable', function() {
       const player = new Player('venky', 'red');
-      const piece = new StablePiece('bomb', [0, 1], false);
-      player.addPiece(piece);
+      player.addPiece('bomb', [0, 1]);
       assert.isFalse(player.movePiece([0, 1], [0, 2]));
     });
   });
+
   context('.killPiece()', function() {
     it('should kill the piece of given position', function() {
       const player = new Player('venky', 'red');
-      const piece = new Piece('marshal', [0, 0], true);
-      player.addPiece(piece);
+      player.addPiece('marshal', [0, 0]);
       assert.isTrue(player.killPiece([0, 0]));
     });
   });
