25c4e4ee9a4b65df33d8662e4973d01e3b2a099c
Bernard Nehal D'Cunha <58025656+bernie-walker@users.noreply.github.com>
2020-03-23 10:11:59 +0530

    |#32|bernie/venky|modified the attack method on Piece
    * created Miner and Spy classes
    * create isSpecialAttack method on Piece

--- test/testPiece.js
+++ test/testPiece.js
@@ -1,10 +1,10 @@
 const assert = require('chai').assert;
-const { Piece } = require('./../lib/piece');
+const { createPiece } = require('./../lib/piece');
 
 describe('Piece', function() {
   context('.getStatus()', function() {
     it('should give the status of the piece', function() {
-      const piece = new Piece('marshal', [0, 0]);
+      const piece = createPiece('marshal', [0, 0]);
       assert.deepStrictEqual(piece.getStatus(), {
         name: 'marshal',
         position: [0, 0],
@@ -15,19 +15,21 @@ describe('Piece', function() {
 
   context('.move()', function() {
     it('should update the position of given piece', function() {
-      const piece = new Piece('marshal', [0, 0]);
+      const piece = createPiece('marshal', [0, 0]);
       const currentPosition = piece.move([9, 0]);
       assert.deepStrictEqual(currentPosition, [9, 0]);
     });
   });
+
   context('.getAttackStatus()', function() {
     it('should return won when the spy attacks the marshal', function() {
-      const piece = new Piece('spy', [0, 0]);
+      const piece = createPiece('spy', [0, 0]);
       const actual = piece.getAttackStatus('marshal');
       assert.deepStrictEqual(actual, 'won');
     });
+
     it('should return won when the miner attacks the bomb', function() {
-      const piece = new Piece('miner', [0, 0]);
+      const piece = createPiece('miner', [0, 0]);
       const actual = piece.getAttackStatus('bomb');
       assert.deepStrictEqual(actual, 'won');
     });
